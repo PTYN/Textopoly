4dc8a27a639a1b4bb01fb474fdd5e85f757df5b2
Lachlan <Leafox22@gmail.com>
2018-03-29 09:34:05 +1100

    get: info property
    Added to command bank file

--- Monopoly command code.py
+++ Monopoly command code.py
@@ -50,12 +50,31 @@ while flag == True:
 		print()
 
 	#giving the player's info
-	elif command == "info":
-		print("Character: ")
-		print("Money: ")
-		print("Properties: ")
-		print()
-	
+	elif command.split()[0] == "info":
+		if command.split()[1] == "player":
+			print("Character: ")
+			print("Money: ")
+			print("Properties: ")
+			print()
+
+		#gives information on specified property
+#		if command.split()[1] == "property":
+#			propsearch = input("What property are you wanting to know about? ")
+#			for i in range(0, len(boardpos)):
+#				if propsearch == boardpos[i][name].lower():
+#					print(boardpos[i][name])
+#					print("- " + boardpos[i][colour].title() + " -")
+#					print("Price: $" + boardpos[i][price])
+#					print("Rent with no buildings: $" + boardpos[i][rent])
+#					print("    \"     one house: $" + boardpos[i][onehouse])
+#					print("    \"     two houses: $" + boardpos[i][twohouse])
+#					print("    \"     three houses: $" + boardpos[i][threehouse])
+#					print("    \"     four houses: $" + boardpos[i][fourhouse])
+#					print("    \"     hotel: $" + boardpos[i][hotel])
+#					print("")
+#					print("Price of single building: $" + boardpos[i][buildprice])
+#					print("")
+
 	#buying the property that they are on
 	elif command == "buy":
 		#if boardpos[playerpos][owner]!= "":
